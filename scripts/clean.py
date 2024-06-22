5084d1d45c05ecf469cc9fbb1c48aa7e8c2588f9
Doodle <doodlebunnyhops@gmail.com>
2024-06-22 11:14:23 -0400

    better script handling

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,16 +1,17 @@
+import urllib.request
+from pathlib import Path
 import json
 import re
 
 
 def load():
+    # Grab from: https://raw.githubusercontent.com/Mojang/bedrock-samples/main/resource_pack/ui/settings_sections/world_section.json
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/Mojang/bedrock-samples/main/resource_pack/ui/settings_sections/world_section.json", "raw.json")
+
     # load JSON data from file
     with open('raw.json') as file:
-        # print(file.read())
-        # data = file.read()
         data = removeComments(file.read())
 
-    # with open('test.json', 'w') as newFile:
-    #     newFile.write(data)
     return json.loads(data)
 
 def write(data,name):
@@ -22,12 +23,8 @@ def write(data,name):
 def removeComments(data):
     data = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,str(data)) # remove all occurrences streamed comments (/*COMMENT */) from string
     data = re.sub(re.compile('[^".*:.*"]//.*\n' ) ,"" , str(data)) # remove all occurrence single-line comments (//COMMENT\n ) from string
-    data = re.sub(re.compile("/\*{2,}" ) ,"" , str(data)) # remove all occurrence single-line comments (//COMMENT\n ) from string
-    data = re.sub(re.compile("^\+\*.*" ) ,"" , str(data)) # remove all occurrence single-line comments (//COMMENT\n ) from string
-
-    # Cleaned version to compared to stripped
-    # with open('4.json', 'w', encoding='utf-8') as f:
-    #     json.dump(json.loads(data), f, sort_keys=False, indent=2)
+    data = re.sub(re.compile("/\*{2,}" ) ,"" , str(data)) # remove all occurrence single-line comments (/** ) from string
+    data = re.sub(re.compile("^\+\*.*" ) ,"" , str(data)) # remove all occurrence single-line comments (+* ) from string
     
     return data
 
@@ -51,25 +48,18 @@ def removeKeys(data):
         print(f"\nLooking for key:\t{key}")
         for index, obj in enumerate(data['game_section']['controls'], start=0):
             if key in obj:
-                print(f"Found Key:\t{str(key)}\n")
+                print(f"Found Key:\t{str(key)}")
                 indexToRemove.append(index)
-                # removed_value = data['game_section']['controls'][index].pop(key)
                 removed_value = data['game_section']['controls'].pop(index)
-                print(f"Removed key '{key}' with value: {removed_value}")
-    
-    # Doing this backwards - misses them if not
-    # print(f"Removing indexes: {indexToRemove}\n")
-    # for found in indexToRemove:
-    #     removed_value = data['game_section']['controls'].pop(max(indexToRemove))
-    #     print(f"Removed key '{found}' with value: {removed_value}")
-
-    # Need to remove {}, left from pop
+                print(f"Removed key '{key}' with value: {removed_value}\n")
 
     # saving the updated JSON data back to the file
-    with open('../HideMySeed/ui/settings_sections/world_section.json', 'w') as file:
+    with open('HideMySeed/ui/settings_sections/world_section.json', 'w') as file:
         json.dump(data, file, indent=2)
 
 
 if __name__ == "__main__":
     data = load()
     removeKeys(data)
+    p = Path('raw.json')
+    Path.unlink(p,True)
